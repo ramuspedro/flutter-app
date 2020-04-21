da93bd2e2f09d7df49684d7887a46dc34fdf1597
Pedro Ramos <ramuspedro@gmail.com>
2020-04-21 16:55:36 -0300

    working on a list

---
+++ lib/category_route.dart
@@ -0,0 +1,93 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hellorectangle/category.dart';
+
+// TODO: Check if we need to import anything
+
+// TODO: Define any constants
+
+/// Category Route (screen).
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
+///
+/// While it is named CategoryRoute, a more apt name would be CategoryScreen,
+/// because it is responsible for the UI at the route's destination.
+class CategoryRoute extends StatelessWidget {
+  const CategoryRoute();
+
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create a list of the eight Categories, using the names and colors
+    // from above. Use a placeholder icon, such as `Icons.cake` for each
+    // Category. We'll add custom icons later.
+
+    // TODO: Create a list view of the Categories
+    final listView = new List<Widget>();
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      listView.add(
+        new Category(
+            name: _categoryNames[i],
+            color: _baseColors[i],
+            iconLocation: Icons.cake),
+      );
+    }
+
+    // TODO: Create an App Bar
+    final appBar = AppBar(
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(
+          fontSize: 30.0,
+          color: Colors.black
+        ),
+      ),
+      elevation: 0.0,
+      backgroundColor: Colors.green[100],
+      centerTitle: true,
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      backgroundColor: Colors.green[100],
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: listView,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
